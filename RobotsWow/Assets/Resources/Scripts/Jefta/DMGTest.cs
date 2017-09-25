d374ed1021fb7572dded3dab74cd87ca9ef1bc79
Jefta Brink <jeftasbrotherhood@gmail.com>
2017-09-25 13:32:26 +0200

    Added Spike Trap not finished

---
+++ RobotsWow/Assets/Resources/Scripts/Jefta/DMGTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DMGTest : MonoBehaviour {
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        
+
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DmgObject")
+        {
+            Debug.Log("kaas");
+        }
+
+
+        
+    }
+}
---
+++ RobotsWow/Assets/Resources/Scripts/Jefta/Trap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public abstract class ITrap : MonoBehaviour
+{
+    public abstract void ActivateTrap();
+    public abstract void ResetTrap();
+    public abstract bool CheckIfFree();
+
+}
