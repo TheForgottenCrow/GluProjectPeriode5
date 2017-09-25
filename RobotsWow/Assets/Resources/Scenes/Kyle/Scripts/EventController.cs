0bc5d9970ca9ff3a067915a9d51bccd299cb2dd4
Kyle van den Boom <520163@student.glu.nl>
2017-09-25 12:38:55 +0200

    Got a trap working, Now i only need to make it dynamic

---
+++ RobotsWow/Assets/Resources/Scenes/Kyle/Scripts/EventController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour
+{
+    List<ITrap> m_traps;
+
+	void Start ()
+    {
+        m_traps = new List<ITrap>();
+
+        m_traps.Add(new PushTrap());
+	}
+}
---
+++ RobotsWow/Assets/Resources/Scenes/Kyle/Scripts/PushTrap.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITrap
+{
+    void Activate();
+    void Reset();
+}
+
+public delegate void OnTrapHit();
+
+public class PushTrap : MonoBehaviour, ITrap
+{
+    public event OnTrapHit TrapHasHitEvent;
+
+    Animator m_animator;
+
+    [SerializeField]
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+
+        m_animator.SetInteger("State", 0);
+
+        TrapHasHitEvent += Reset;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Activate();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            Reset();
+        }
+    }
+
+    public void Activate()
+    {
+        m_animator.SetInteger("State", 1);
+    }
+
+    public void Reset()
+    {
+        m_animator.SetInteger("State", 0);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(TrapHasHitEvent != null)
+        {
+            if(other.tag == "Player1")
+            {
+                Debug.Log("BOEM!!!");
+
+                TrapHasHitEvent();
+            }
+        }
+    }
+}
